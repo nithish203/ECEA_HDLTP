a8d76faf56dc3548ceb1c955907679f192b21770
nithish203 <115063170+nithish203@users.noreply.github.com>
2022-10-15 12:56:25 +0530

    Update two_bit_adder_tb.v

--- Week2/two_bit_adder_tb.v
+++ Week2/two_bit_adder_tb.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.10.2022 00:56:52
-// Design Name: 
-// Module Name: adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module adder_tb(
     );
